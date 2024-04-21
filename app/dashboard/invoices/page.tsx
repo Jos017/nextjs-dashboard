6ac32db98160bc82a8fccc934d98ceec0faa51db
Jose Rios <jrbernabe@gmail.com>
2024-04-21 14:44:35 -0400

    Chapter 16: Adding Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,12 +6,19 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
-export default async function Page({searchParams}: {
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page({
+  searchParams,
+}: {
   searchParams?: {
     query?: string;
-    page?:string;
-  }
+    page?: string;
+  };
 }) {
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page || 1);
